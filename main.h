5230217678d6f4a49942ccbdc9c4363c1c7379cb
72119sisay <sisayfikru69@gmail.com>
2022-10-21 18:00:06 -0700

    printf

--- main.h
+++ main.h
@@ -86,6 +86,10 @@ int get_size(const char *format, int *i);
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+{
+	(void)buffer;
+	return 0;
+}
 
 /*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
